7554126ae885124e36c04f6fc3abca663da4e3dd
David Hope <djhope@btinternet.com>
2025-01-08 16:04:22 -0500

    add otel annotations

--- resources/edot_2_workshop.py
+++ resources/edot_2_workshop.py
@@ -23,26 +23,8 @@ def deploy_three_tiered_java():
     service_file = 'three-tier-java-app/services.yaml'
 
     try:
-        # Read and modify deployment file to add OpenTelemetry annotations
-        with open(deployment_file, 'r') as f:
-            deployments = list(yaml.safe_load_all(f))
-
-        # Add OpenTelemetry annotations to each deployment (except load-generator)
-        for deployment in deployments:
-            if deployment['metadata']['name'] != 'load-generator':
-                if 'annotations' not in deployment['spec']['template']['metadata']:
-                    deployment['spec']['template']['metadata']['annotations'] = {}
-                deployment['spec']['template']['metadata']['annotations'].update({
-                    'instrumentation.opentelemetry.io/inject-java': 'opentelemetry-operator-system/elastic-instrumentation'
-                })
-
-        # Write modified deployments to a temporary file
-        modified_deployment_file = 'three-tier-java-app/modified-deployment.yaml'
-        with open(modified_deployment_file, 'w') as f:
-            yaml.safe_dump_all(deployments, f)
-
         # Apply the configurations
-        subprocess.run(['kubectl', 'apply', '-f', modified_deployment_file], check=True)
+        subprocess.run(['kubectl', 'apply', '-f', deployment_file], check=True)
         subprocess.run(['kubectl', 'apply', '-f', service_file], check=True)
         
         print("Java favorite application deployed successfully with OpenTelemetry instrumentation.")
